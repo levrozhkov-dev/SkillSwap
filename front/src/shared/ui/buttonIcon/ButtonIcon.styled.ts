b5516451c9f911a62b7c1369caeb295ee104ff33
Thblslvr <tolebasrudenko@yandex.ru>
2026-01-19 12:50:42 +0500

    разделены стили и компонент, удалены лишние svg

---
+++ front/src/shared/ui/buttonIcon/ButtonIcon.styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+`;
+
+export const IconImage = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+`; 
