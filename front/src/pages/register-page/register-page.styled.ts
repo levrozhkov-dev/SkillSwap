5f2cf172cfe7c9d51eac1d40c7376677693b0eae
dmitry-serov <frickmen@yandex.ru>
2026-01-25 13:10:30 +0300

    feat: реализовать страницу регистрации (шаг 1)

---
+++ front/src/pages/register-page/register-page.styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MainContent = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  background: var(--color-bg-main);
+`;
+
+export const StepProgressWrapper = styled.div`
+  padding: 24px 0;
+`;
--- front/src/pages/register-page/register-page.tsx
+++ front/src/pages/register-page/register-page.tsx
@@ -1,5 +1,37 @@
 import type { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Header } from '../../widgets/Header';
+import { Footer } from '../../widgets/Footer';
+import { StepProgress } from '../../shared/ui/StepProgress';
+import { RegisterFormStepOne } from '../../widgets/RegisterForm/ui';
+import * as S from './register-page.styled';
 
 export const RegisterPage: FC = () => {
-  return <div>Это страница регистрации (RegisterPage)</div>;
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    if (window.history.length > 1) {
+      navigate(-1);
+    } else {
+      navigate('/');
+    }
+  };
+
+  const handleStepOneSubmit = (data: { email: string; password: string }) => {
+    console.log('Шаг 1 завершён:', data);
+    // TODO: сохранить данные и перейти на шаг 2
+  };
+
+  return (
+    <S.PageWrapper>
+      <Header />
+      <S.MainContent>
+        <S.StepProgressWrapper>
+          <StepProgress currentStep={1} totalSteps={3} />
+        </S.StepProgressWrapper>
+        <RegisterFormStepOne onSubmit={handleStepOneSubmit} />
+      </S.MainContent>
+      <Footer />
+    </S.PageWrapper>
+  );
 };
